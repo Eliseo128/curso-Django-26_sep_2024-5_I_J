147a4e018c72685c76d53d0363746cc4cfd1f5a2
Manuel Nava <navaweb@cbtis128.edu.mx>
2024-09-26 03:51:27 -0700

    Add files via upload
    video del 1 al 31 curso de jango

---
+++ empleadoapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    
+    path('',index_empleadoapp), #127.0.0.1:8000
+    path('crear',crear_empleadoapp) #127.0.0.1:8000/crear
+]
